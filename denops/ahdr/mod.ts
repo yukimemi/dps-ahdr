d98ed37ba8909ead3cd97d22814636fab65b3235
yukimemi <yukimemi@gmail.com>
2021-03-07 20:33:28 +0900

    :zap: Fix encode and ff, and works on Windows.

--- denops/ahdr/mod.ts
+++ denops/ahdr/mod.ts
@@ -1,11 +1,11 @@
 import * as path from "https://deno.land/std@0.89.0/path/mod.ts";
 import { parse } from "https://deno.land/std@0.89.0/encoding/toml.ts";
 import { start } from "https://deno.land/x/denops_std@v0.3/mod.ts";
+import { isWindows } from "https://deno.land/std@0.89.0/_util/os.ts";
 
 start(async (vim) => {
-  // Config load.
-  // this file's path.
-  const dir = new URL(".", import.meta.url).pathname;
+  const pathname = new URL(".", import.meta.url).pathname;
+  const dir = isWindows ? pathname.slice(1) : pathname;
   const toml = path.join(dir, "config.toml");
   const cfg = parse(await Deno.readTextFile(toml));
 
@@ -33,40 +33,45 @@ start(async (vim) => {
       );
       console.log(`header: ${h.header}`);
 
-      // Get buffer file path.
-      const infile = (await vim.call("expand", "%:p")) as string;
-      const buf = await Deno.readTextFile(infile);
+      // Get buffer info.
+      const inpath = (await vim.call("expand", "%:p")) as string;
+      const fenc = (await vim.eval("&fenc")) as string;
+      const ff = (await vim.eval("&ff")) as string;
+      const lz = (await vim.eval("&lazyredraw")) as string;
+      const bname = (await vim.call("bufname")) as string;
 
-      // Merge out.
-      const outfile = path.join(
-        path.dirname(infile),
-        `${h.prefix}${path.basename(infile, path.extname(infile))}${h.suffix}${
+      const lines = (await vim.call("getline", 1, "$")) as string[];
+      const outbuf = `${h.header}\n${lines.join("\n")}`;
+
+      // Get output path.
+      const outpath = path.join(
+        path.dirname(inpath),
+        `${h.prefix}${path.basename(inpath, path.extname(inpath))}${h.suffix}${
           h.ext
         }`
       );
 
-      console.log(`infile: ${infile}`);
-      console.log(`outfile: ${outfile}`);
+      console.log(`inpath: ${inpath}`);
+      console.log(`outpath: ${outpath}`);
 
-      await Deno.writeTextFile(outfile, `${h.header}\n`, { create: true });
+      console.log(`Set fenc: ${fenc}, ff: ${ff} to ${outpath}`);
 
-      const fenc = (await vim.eval("&fenc")) as string;
-      const ff = (await vim.eval("&ff")) as string;
-      const bname = (await vim.call("bufname")) as string;
-
+      await vim.cmd(`set lazyredraw`);
+      await vim.cmd(`edit ${outpath}`);
+      await vim.call("setline", 1, outbuf.split(/\r?\n/g));
       // Fix fenc and ff.
       await vim.execute(`
-        edit ${outfile}
         setlocal fenc=${fenc}
         setlocal ff=${ff}
-        update
+        write
         bwipeout
         buffer ${bname}
       `);
+      if (!lz) {
+        await vim.cmd(`set nolazyredraw`);
+      }
 
-      await Deno.writeTextFile(outfile, buf, { append: true });
-
-      return await Promise.resolve(`Write [${outfile}]`);
+      return await Promise.resolve(`Write [${outpath}]`);
     },
   });
 
