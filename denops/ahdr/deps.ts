e78748022351b5f4a4574f31dedd0e1705af5790
yukimemi <yukimemi@gmail.com>
2021-07-11 23:55:55 +0900

    :up: Update denops_std.

--- denops/ahdr/deps.ts
+++ denops/ahdr/deps.ts
@@ -5,9 +5,9 @@ export * as io from "https://deno.land/std@0.100.0/io/mod.ts";
 export * as path from "https://deno.land/std@0.100.0/path/mod.ts";
 export * as toml from "https://deno.land/std@0.100.0/encoding/toml.ts";
 
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.5/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.5/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v1.0.0-beta.5/function/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.5/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.7/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.7/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v1.0.0-beta.7/function/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.7/helper/mod.ts";
 
 export { ensureString } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
